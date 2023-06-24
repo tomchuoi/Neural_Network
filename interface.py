47004e5ae03da32632423be82c3394342ab2ccd5
Bạch Ngọc Hưng <45124700+tomchuoi@users.noreply.github.com>
2023-06-24 22:36:36 +0700

    Update interface.py

--- interface.py
+++ interface.py
@@ -2,13 +2,8 @@ from model import Model
 from image_classifier import ImageClassifierUI
 
 if __name__ == "__main__":
-    # Load the trained model
     model = Model.load('trained.model')
-
-    # Create an instance of the ImageClassifierUI class
     image_classifier_ui = ImageClassifierUI(model)
-
-    # Start the main loop
     image_classifier_ui.root.mainloop()
 
 # Start the main loop
